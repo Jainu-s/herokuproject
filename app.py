162a3d32e5cce1acdd180dfb4d44c9a2bc472b15
Jainmiah Shaik <59506207+Jainu-s@users.noreply.github.com>
2020-06-11 10:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__ , template_folder='.')
+app = Flask(__name__)
 model = pickle.load(open('MultiLiModel.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html',whiskey = 42)
+    return render_template('index.html')
 
 
 output = []
